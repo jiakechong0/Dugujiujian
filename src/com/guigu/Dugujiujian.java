de7c3a6cd1173f267918657ba2d7d14a56855f11
Administrator <Administrator@QIANQIANCHEN>
2016-10-31 14:47:00 +0800

    update 02

--- src/com/guigu/Dugujiujian.java
+++ src/com/guigu/Dugujiujian.java
@@ -1,7 +1,14 @@
 package com.guigu;
 
+import java.util.Date;
+
 public class Dugujiujian {
 	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date);
+		System.out.println(date);
+		System.out.println(date);
+		System.out.println(date);
 		
 	}
 
