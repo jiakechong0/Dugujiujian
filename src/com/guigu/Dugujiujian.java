babf4eb7ee7ee47a98fc6fba2fe53ce1f4bcb48f
ChenQianqian0 <593689178@qq.com>
2016-11-01 06:12:14 +0800

    update03

--- src/com/guigu/Dugujiujian.java
+++ src/com/guigu/Dugujiujian.java
@@ -5,13 +5,7 @@ import java.util.Date;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		Date date = new Date();
-		System.out.println(date);
-		System.out.println(date);
 		System.out.println("1111");
-		System.out.println(date);
-		System.out.println(date);
-		System.out.println(date);
-		System.out.println(date);
 		System.out.println("new day");
 		
 	}
