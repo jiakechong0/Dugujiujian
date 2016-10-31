72fdbde762475d1f6d57bd9caa88b984329a4669
jiakechong0 <1583167271@qq.com>
2016-11-01 06:45:10 +0800

    Update Dugujiujian.java

--- src/com/guigu/Dugujiujian.java
+++ src/com/guigu/Dugujiujian.java
@@ -6,7 +6,8 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		Date date = new Date();
 		System.out.println("1111");
-		System.out.println("new day02");
+		System.out.println("new day");
+		System.out.println("new day11");
 		
 	}
 
